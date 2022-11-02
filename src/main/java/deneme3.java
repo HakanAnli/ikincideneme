7f7acedf17eab542cc3bcaa90f5e6c314dbd3f5a
HakanAnli <hakananli4@gmail.com>
2022-11-02 23:55:45 +0300

    deneme5

--- src/main/java/deneme3.java
+++ src/main/java/deneme3.java
@@ -2,6 +2,9 @@ public class deneme3 {
 
     public static void main(String[] args) {
         System.out.println("Hello Team08");
+        System.out.println("Hello Team08");
+        System.out.println("Hello Team08");
+        System.out.println("Hello Team08");
     }
 
 }
